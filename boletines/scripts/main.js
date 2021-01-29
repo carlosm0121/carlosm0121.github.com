d4e7394b69f6c654aac89cc04de5b15ab0388e3e
carlos <cjmscjms21@gmail.com>
2021-01-29 00:20:21 -0600

    m

--- boletines/scripts/main.js
+++ boletines/scripts/main.js
@@ -1,6 +1,7 @@
-console.log("hola mun")
-
 //DOM
 //queryselector es solo para el primero--queryselectorAll es para todos los elementos
 let links = document.querySelectorAll("a");
-console.log(links)
+
+links.forEach(function(Links){
+  console.log(links);
+})
