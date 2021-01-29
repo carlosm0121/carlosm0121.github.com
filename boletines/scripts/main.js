c1c0e425123fe35b99f0d0cc61ae65c086ab10d1
carlos <cjmscjms21@gmail.com>
2021-01-29 00:23:13 -0600

    click

--- boletines/scripts/main.js
+++ boletines/scripts/main.js
@@ -1,7 +1,15 @@
 //DOM
 //queryselector es solo para el primero--queryselectorAll es para todos los elementos
-let links = document.querySelectorAll("a");
+// let links = document.querySelectorAll("a");
+//
+// links.forEach(function(Links){
+//   console.log(links);
+// })
 
-links.forEach(function(Links){
-  console.log(links);
-})
+let td =  document.querySelectorAll("a");
+
+td.forEach(function(td){
+  td.addEventListener('click', function(){
+    console.log("click");
+  })
+});
